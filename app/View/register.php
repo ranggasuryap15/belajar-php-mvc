3026f603284bd154866d61a813630ab2120a9d65
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-07-22 17:13:15 +0700

    init commit

---
+++ app/View/register.php
@@ -0,0 +1,6 @@
+<html>
+
+<body>
+    <h1>Register</h1>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// set default path = index
+$path = '/index';
+
+if (isset($_SERVER['PATH_INFO'])) {
+    $path = $_SERVER['PATH_INFO'];
+}
+
+require __DIR__ . '/../app/View' . $path . '.php';
